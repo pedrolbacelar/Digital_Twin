3e3a59cfabc2fdc3cca8a940aac6014d86d3c6a7
alexlona <clonaalex@gmail.com>
2023-01-10 14:58:39 +0100

    2s model machine and queue

--- Parallel_test/2-s model/2-s model.py
+++ Parallel_test/2-s model/2-s model.py
@@ -1,26 +1,54 @@
 import simpy
 
 class Machine:
-    def __init__(self, env, name, queue_in, queue_out):
+    def __init__(self, env, id, queue_in, queue_out, process_time, capacity, blocking_policy):
         self.env = env
-        self.name = name
+        self.id = id
+        self.name = 'machine_'+str(id)
         self.queue_in = queue_in
         self.queue_out = queue_out
-        self.action = env.process(self.run())
+        self.process_time = process_time
+        self.capacity = capacity
+        self.blocking_policy = blocking_policy
+        
+        
 
     def run(self):
         while True:
             resource = yield self.queue_in.get()
-            print(f'{self.name} got resource at {env.now}')
-            yield env.timeout(5)  # processing time
+
+            print(f'{self.name} got resource at {self.env.now}')
+
+            
+
+            # blocking policy for Blocking Before Service (BBS)
+            if self.blocking_policy == 'BBS':
+                while self.queue_out.get_len()>=self.queue_out.capacity:
+                    yield
+
+
+            # processinf of the part depending on part type
+                        
+            yield env.timeout(self.process_time[resource.get_type()])  # processing time stored in a dictionary
             self.queue_out.put(resource)
+
+            # blocking policy for Blocking After Service (BAS)
+            if self.blocking_policy == 'BAS':
+                while self.queue_out.get_len()>=self.queue_out.capacity:
+                    yield
+
             print(f'{self.name} put resource in {self.queue_out.name} at {env.now}')
 
 class Queue:
-    def __init__(self, env, name, capacity):
+    def __init__(self, env, id, capacity, machine_in, machine_out):
         self.env = env
-        self.name = name
+        self.id = id
+        self.name = 'queue_'+str(id)
         self.store = simpy.Store(env, capacity=capacity)
+        self.capacity = capacity
+        self.machine_in = machine_in
+        self.machine_out = machine_out
+        self.queue_strength = None   # add initial condition
 
     def put(self, resource):
         return self.store.put(resource)
@@ -28,14 +56,48 @@ class Queue:
     def get(self):
         return self.store.get()
 
-        
+    def get_all_items(self):
+        return self.store.items 
+
+    def get_len(self):
+        self.queue_strength = len(self.store.items)
+        return self.queue_strength
+
+
+class Part:
+    def __init__(self, name, type):
+        self.name = name
+        self.type = type
+    
+    def get_type(self):
+        return self.type
+    
 
 env = simpy.Environment()
 
-queue1 = Queue(env, 'Queue 1', capacity=float('inf'))
-queue2 = Queue(env, 'Queue 2', capacity=float('inf'))
+process_time_1 = {'type_A':5, 'type_B':15}
+process_time_2 = {'type_A':8, 'type_B':17}
 
-machine1 = Machine(env, 'Machine 1', queue1, queue2)
-machine2 = Machine(env, 'Machine 2', queue2, queue1)
+part1 = Part("Part1", "type_A")
 
-env.run(until=20)
+queue1 = Queue(env, 1, float('inf'), 'machine_1', 'machine_2')
+queue2 = Queue(env, 2, float('inf'), 'machine_2', 'machine_1')
+
+machine1 = Machine(env,1, queue1, queue2, process_time_1, 1, 'BAS')
+machine2 = Machine(env,2, queue2, queue1, process_time_2, 1, 'BAS')
+# machine2 = Machine(env, 'Machine 2', queue2, queue1) #old
+
+env.process(machine1.run())
+env.process(machine2.run())
+
+def generator(env, queue):
+    while True:
+        yield env.timeout(3)
+        part = Part("Part {}".format(env.now), type="type_A")
+        queue1.put(part)
+
+
+env.process(generator(env, Queue))
+
+env.run(until=200)
+
