63d7b6d98e996ce10365b4700a127a0df2126d71
alexlona <clonaalex@gmail.com>
2023-03-04 17:00:56 +0100

    arduinoNano for RFID

--- RFID_Arduino/test_nano/test_nano.ino
+++ RFID_Arduino/test_nano/test_nano.ino
@@ -1,7 +1,14 @@
 void setup(){
-
+Serial.begin(9600);
+pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop(){
-    
+    for (int x=1;x<=2;x++){
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(200);
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(200);
+    }
+    delay(1000);
 }
