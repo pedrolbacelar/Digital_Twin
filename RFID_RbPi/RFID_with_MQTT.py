2488ec198591b2cc5e0ba0aca7e4ce91fa51f508
alexlona <clonaalex@gmail.com>
2023-03-06 23:41:43 +0100

    write part id to the RFID

--- RFID_RbPi/RFID_with_MQTT.py
+++ RFID_RbPi/RFID_with_MQTT.py
@@ -1,10 +1,14 @@
+#--- read rfid tag and send the part id
+#--- part id is different from the tag id which is unique.
+#--- station 1 RFID reader assigns part_id to the tag (reader.write())
+
 
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 
 reader = SimpleMFRC522()
 last_id = 000000
-
+part_id = 1
 try:
     def on_connect(client, userdata, flags, rc):
         print("Connected with result code "+str(rc))
@@ -17,10 +21,12 @@ try:
     while True:
         id, text = reader.read()
         if id != last_id:
-            print("id : ",id)
-            payload ={"machine_id" : "1", "part_id":id}
+            print("id : ",id, "part_id : ", part_id)
+            reader.write(part_id)   # write the part_id to the tag.
+            payload ={"machine_id" : "1", "part_id":part_id}
             client.publish(topic = "part_id", payload= json.dumps(payload))
             last_id = id
+            part_id += 1
         else:
             print("same part detected again")
 finally:
