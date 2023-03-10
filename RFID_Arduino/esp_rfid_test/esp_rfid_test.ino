014214fa3ac6d11e9f408639f416aaf3406e8457
Pedro Bacelar <pedrobacelarsantos@usp.br>
2023-03-10 23:17:25 +0100

    ESP32 done

--- RFID_Arduino/esp_rfid_test/esp_rfid_test.ino
+++ RFID_Arduino/esp_rfid_test/esp_rfid_test.ino
@@ -1,20 +1,7 @@
 /*
- * --------------------------------------------------------------------------------------------------------------------
- * Example sketch/program showing how to read new NUID from a PICC to serial.
- * --------------------------------------------------------------------------------------------------------------------
- * This is a MFRC522 library example; for further details and other examples see: https://github.com/miguelbalboa/rfid
- *
- * Example sketch/program showing how to the read data from a PICC (that is: a RFID Tag or Card) using a MFRC522 based RFID
- * Reader on the Arduino SPI interface.
- *
- * When the Arduino and the MFRC522 module are connected (see the pin layout below), load this sketch into Arduino IDE
- * then verify/compile and upload it. To see the output: use Tools, Serial Monitor of the IDE (hit Ctrl+Shft+M). When
- * you present a PICC (that is: a RFID Tag or Card) at reading distance of the MFRC522 Reader/PCD, the serial output
- * will show the type, and the NUID if a new card has been detected. Note: you may see "Timeout in communication" messages
- * when removing the PICC from reading distance too early.
- *
- * @license Released into the public domain.
- *
+ * ---------------------------------------------------------------------------------------------------------------------------
+ * RFID READER: The ESP32 has a RFID reader (RC522) to read RFID stickers UID (unique ID) and sends the UID through MQTT
+ * ---------------------------------------------------------------------------------------------------------------------------
  * Typical pin layout used:
  * -----------------------------------------------------------------------------------------
  *             MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
@@ -27,42 +14,49 @@
  * SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
  * SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
  *
- * More pin layouts for other boards can be found here: https://github.com/miguelbalboa/rfid#pin-layout
  */
 
-#include <SPI.h>
-#include <MFRC522.h>
-#include <WiFi.h>
-#include <PubSubClient.h>
-
+// ####################### WIFI & IP ADDRESS CONFIGURATION #######################
 // lego factory credentials
 const char *ssid = "THE FACTORY - ROUTER 2";
 const char *password = "legofactory";
 const char *mqtt_server = "192.168.0.50";
 
 // my pc credentials
-// const char* ssid = "Galaxy A22DB0F";
-// const char* password = "tpqv9051";
-// const char* mqtt_server = "192.168.2.70";
+//const char* ssid = "Galaxy A22DB0F";
+//const char* password = "tpqv9051";
+//const char* mqtt_server = "192.168.15.41";
+// ##################################################################################
 
+
+// ################################### MACHINE ID ###################################
+String machine_id = "1";
+// ##################################################################################
+
+// ====================================================================================================================================================
+
+// ------------- Libraries -------------
+#include <SPI.h>
+#include <MFRC522.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+// -------------------------------------
+
+// -------------- Variables Config --------------
 WiFiClient wifiClient;
 PubSubClient client(wifiClient);
-
 #define SS_PIN 5
 #define RST_PIN 22
-
 MFRC522 rfid(SS_PIN, RST_PIN);  // Instance of the class
-
 MFRC522::MIFARE_Key key;
-
-// Init array that will store new NUID
 byte nuidPICC[4];
+// -----------------------------------------
 
-// counter variable
-int count = 1;
+// -------------------------------------- xxxxxxxxx -------------------------------------------------
 
 void setup() {
   Serial.begin(115200);
+  Serial.println("Hello from ESP32");
   SPI.begin();      // Init SPI bus
   rfid.PCD_Init();  // Init MFRC522
 
@@ -79,21 +73,29 @@ void setup() {
   Serial.println(F("This code scan the MIFARE Classsic NUID."));
 }
 
-void loop() {
+// -------------------------------------- xxxxxxxxx -------------------------------------------------
+
+void loop() 
+{
   if (!client.connected()) {
     Serial.println("not connected");
     reconnect();
   }
   client.loop();
 
+  // -------------------------------------------- RESET THE LOOP --------------------------------------------
   // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
   if (!rfid.PICC_IsNewCardPresent())
     return;
+  // ---------------------------------------------------------------------------------------------------------
 
+  // -------------------------------------------- RESET THE LOOP --------------------------------------------
   // Verify if the NUID has been readed
   if (!rfid.PICC_ReadCardSerial())
     return;
+  // ---------------------------------------------------------------------------------------------------------
 
+  // ----------------------------------------------- MAIN FUNCTION --------------------------------------------
   if (rfid.uid.uidByte[0] != nuidPICC[0] || rfid.uid.uidByte[1] != nuidPICC[1] || rfid.uid.uidByte[2] != nuidPICC[2] || rfid.uid.uidByte[3] != nuidPICC[3]) {
     Serial.println();
     Serial.println(F("New card"));
@@ -103,79 +105,25 @@ void loop() {
       nuidPICC[i] = rfid.uid.uidByte[i];
     }
 
-    // ---------------------- WITHOUT VARIABLE ----------------------
-
-    // String RFID_ID = printDec(rfid.uid.uidByte, rfid.uid.size);
-    // Serial.println(RFID_ID);
-
-    // ----------------------- WITH VARIABLE -----------------------
+    // ----------------------- PAYLOAD CONSTRUCTION -----------------------
     String RFID_ID = printDec(rfid.uid.uidByte, rfid.uid.size);
     Serial.println(RFID_ID);
-    String RFID_ID_dict = "{'machine_id':'1','part_id':";
-    RFID_ID_dict.concat("'");
+    String RFID_ID_dict = "{'machine_id':'";
+    RFID_ID_dict.concat(machine_id);
+    RFID_ID_dict.concat("','part_id': '");
     RFID_ID_dict.concat(RFID_ID);
-    RFID_ID_dict.concat("'");
-    RFID_ID_dict.concat("}");
+    RFID_ID_dict.concat("'}");
     char payload[RFID_ID_dict.length() + 1];
     RFID_ID_dict.toCharArray(payload, sizeof(payload));
-    client.publish("part_id", payload);
+    // --------------------------------------------------------------------
 
-    //--------------------------------------------------------------
-
-    // switch (RFID_ID)
-    // {
-    // case "22019510149":
-    //   client.publish("part_id", "{'machine_id':'1','part_id':'22019510149'}");
-    //   break;
-    // case "1889810149":
-    //   client.publish("part_id", "{'machine_id':'1','part_id':'1889810149'}");
-    //   break;
-    // case "2049810149":
-    //   client.publish("part_id", "{'machine_id':'1','part_id':'2049810149'}");
-    //   break;
-    // case "2209210149":
-    //   client.publish("part_id", "{'machine_id':'1','part_id':'2209210149'}");
-    //   break;
-    // case "20419710149":
-    //   client.publish("part_id", "{'machine_id':'1','part_id':'20419710149'}");
-    //   break;
-    // default:
-    //   client.publish("part_id", "{'machine_id':'1','part_id':'111'}");
-    //   break;
-    // }
-
-    // Old Implementation
-    //    for (int c = 0; c < 4; c++)
-    //    {
-    //    if (RFID_ID == "22019510149")
-    //    {
-    //      client.publish("part_id", "{'machine_id':'3','part_id':'1'}");
-    //      }
-    //    else if (RFID_ID == "1889810149")
-    //    {
-    //      client.publish("part_id", "{'machine_id':'3','part_id':'2'}");
-    //    }
-    //    else if (RFID_ID == "2049810149")
-    //    {
-    //      client.publish("part_id", "{'machine_id':'3','part_id':'3'}");
-    //      }
-    //    else if (RFID_ID == "2209210149")
-    //    {
-    //      client.publish("part_id", "{'machine_id':'3','part_id':'4'}");
-    //      }
-    //    else if (RFID_ID == "20419710149")
-    //    {
-    //      client.publish("part_id", "{'machine_id':'3','part_id':'5'}");
-    //      }
-    //    else
-    //    {
-    //      client.publish("part_id", "{'machine_id':'3','part_id':'111'}");
-    //      }
-
-    //   delay(500);
-    //   }
-    //   //count += 1;
-    // }
+    // --------------------------- PUBLISHING -----------------------------
+    for (int c = 0; c < 3; c++)
+    {
+      client.publish("part_id", payload);
+      delay(500);
+    }
+    // ---------------------------------------------------------------------
 
   } else Serial.println(F("Card repeated."));
 
@@ -185,7 +133,10 @@ void loop() {
   // Stop encryption on PCD
   rfid.PCD_StopCrypto1();
 }
+// ---------------------------------------------------------------------------------------------------------
 
+
+// ---------------------------------------------- EXTERNAL FUNCTIONS ----------------------------------------------
 void reconnect() {
   while (!client.connected()) {
     Serial.print("inside reconnect");
@@ -211,10 +162,6 @@ String printDec(byte *buffer, byte bufferSize) {
     result += (String)buffer[i];
   }
   return result;
-  // for (byte i = 0; i < bufferSize; i++) {
-  //   Serial.print(buffer[i] < 0x10 ? " 0" : " ");
-  //   Serial.print(buffer[i], DEC);
-  //   Serial.print((int)buffer[i]);
-  // }
 }
+// ---------------------------------------------------------------------------------------------------------
 
