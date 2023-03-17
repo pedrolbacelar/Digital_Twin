8295be1efb6b6c2ad9f8cfa8495a35aeaf17038d
alexlona <clonaalex@gmail.com>
2023-03-17 12:44:56 +0100

    small test

--- Digital Twin/mydt.py
+++ Digital Twin/mydt.py
@@ -3,7 +3,7 @@ print("Importing the libraries.....")
 from dtwinpylib.dtwinpy.Digital_Twin import Digital_Twin
 
 #--- Create a Digital Twin object with the require inputs
-mydt = Digital_Twin(name= "5s_determ", template= True, Freq_Sync= 20, Freq_Valid= 60, Freq_Service= 10000, delta_t_treshold=40)
+mydt = Digital_Twin(name= "5s_determ", template= True, Freq_Sync= 60, Freq_Valid= 60, Freq_Service= 10000, delta_t_treshold=40)
 
 #--- Run the real time Digital Twin
 mydt.run()
