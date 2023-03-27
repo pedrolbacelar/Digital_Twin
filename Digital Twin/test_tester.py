6b93627d278167579ee7cf84e8575a76ebf18039
alexlona <clonaalex@gmail.com>
2023-03-27 05:02:49 +0200

    tester.py for loading and setting test data

---
+++ Digital Twin/test_tester.py
@@ -0,0 +1,8 @@
+from dtwinpylib.dtwinpy.tester import tester
+test = tester()
+
+#--- create allexp databse
+# test.create_allexp_database()
+
+test = tester()
+print(f"'{test.exp_id}' experiment is loaded")
