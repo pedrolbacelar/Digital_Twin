5060d81fa250dc320afdadbe5893228aa43a3b48
Pedro Bacelar <pedrobacelarsantos@usp.br>
2023-05-18 10:50:43 +0100

    update of the library

--- Digital Twin/dtwinpylib/setup.py
+++ Digital Twin/dtwinpylib/setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name='dtwinpy',
     packages=find_packages(),
-    version= '0.0.3.9',
-    description= 'First Functional mydt.run',
+    version= '0.1.0',
+    description= 'Complete DT running',
     author='Pedro Bacelar and Alex',
     license='MIT'
 )
